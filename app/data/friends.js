8954dd232f86518b780b81f5e27bb576c1dbf3a0
Samuel-Hall <samuel.r.hall14@gmail.com>
2018-08-13 19:18:04 -0400

    Done

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,7 +14,39 @@ var friends = [
             4,
             1
         ]
-    }
+    },
+    {
+        "name": "Wario",
+        "photo": "https://vignette.wikia.nocookie.net/sonic/images/9/9c/WarioMPDS.png/revision/latest?cb=20160312133123",
+        "scores": [
+            3,
+            1,
+            5,
+            2,
+            3,
+            5,
+            5,
+            4,
+            2,
+            1
+        ]
+    },
+    {
+        "name": "Donkey Kong",
+        "photo": "https://cdn.vox-cdn.com/thumbor/9hToNa31_Qgi50eUgO4PhjsKl9c=/0x0:1423x680/1200x675/filters:focal(599x227:825x453)/cdn.vox-cdn.com/uploads/chorus_image/image/59681935/dk.0.png",
+        "scores": [
+            4,
+            4,
+            5,
+            5,
+            3,
+            5,
+            1,
+            1,
+            5,
+            4
+        ]
+    },
 ];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,12 +6,39 @@ module.exports = function(app) {
       });
     app.post("/api/survey", function(req, res) {
         var newSurvey = req.body;
-        console.log(newSurvey);
+        var userScores = newSurvey.scores;
+        var bestFriend;    
+        function convertScores() {
+            for (var i = 0; i < userScores.length; i++) {
+                userScores[i] = parseInt(userScores[i]);
+            }
+        }
+        function compatibility() {
+            console.log("Hold tight");
+            var totalDiffArray = [];
+            var minTotalDiff;
+            var minDiffIndex;
+            for (var j = 0; j < friends.length; j++) {
+                var totalDifference = 0;
+                // compare scores
+                for (var k = 0; k < userScores.length; k++) {
+                    totalDifference += Math.abs(userScores[k] - friends[j].scores[k]);
+                }
+                totalDiffArray.push(totalDifference);
+                console.log("Total differnce array: " + totalDiffArray);
+            }
+            // find the index of the smallest number in the totalDiffArray
+            minTotalDiff = Math.min.apply(Math, totalDiffArray);
+            console.log("Minimum total difference: " + minTotalDiff);
+            minDiffIndex = totalDiffArray.indexOf(minTotalDiff);
+            console.log("Index of minimum: " + minDiffIndex);
+            bestFriend = friends[minDiffIndex];
+            console.log("Your best friend is: " + bestFriend.name);
+            
+        }
+        convertScores();
         compatibility();
         friends.push(newSurvey);
-        res.json(newSurvey);
-    });
-    app.get("/api/compatibility", function(req, res) {
-        return res.json(bestFriend);
-    });
+        res.json(bestFriend);
+    });  
 }
